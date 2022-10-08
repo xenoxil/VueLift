816c75d3d32b4482e64531bc864a098b7e42786c
Xenoxil <xilus1127@gmail.com>
2022-10-08 23:28:33 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<template>
+<section class="section__lift"> 
+  <div class="floor">
+    <div class="lift__shaft"></div>
+    <button class="lift__btn"></button>
+  </div>
+  
+</section>
+</template>
+
+<style >
+*{margin:0;
+padding: 0;
+box-sizing: border-box;}
+.section__lift{
+  width: 100vw;
+  height:100vh;
+background-color: rgb(231, 231, 231);
+}
+.floor{
+  width:100px;
+  min-height: 100px;
+}
+.lift__shaft{
+  margin-left:15px;
+  width:100%;
+  min-height: 100%;
+  border-left:  1px solid gray;
+  border-right:  1px solid gray;
+}
+</style>
