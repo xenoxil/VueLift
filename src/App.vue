87e81d69e760aebee32f153dda44a908487aade4
Xenoxil <xilus1127@gmail.com>
2022-10-14 16:51:07 +0300

    сделать хранилище localstorage для предыдущего состояния

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@ export default {
   data() {
     return {
       floors: 5,
-      shafts: 1,
-      liftCallStack: [],      
+      shafts: 3,
+      liftCallStack: [],
       liftOnFloor: 1,
       liftPosition: {
         transition: 'all 3s linear 0s',
@@ -20,13 +20,14 @@ export default {
     };
   },
   methods: {
-    callLift(floor) {      
-      if(this.liftOnFloor!==floor){
+    callLift(floor) {
+      if (this.liftOnFloor !== floor) {
         this.liftCallStack.push(floor);
-        if (this.isLiftFree) {          
-        this.moveLiftToFloor(floor);
-      }  
-      }          
+        localStorage.setItem('liftCallStack', this.liftCallStack);
+        if (this.isLiftFree) {
+          this.moveLiftToFloor(floor);
+        }
+      }
     },
     moveLiftToFloor(floor) {
       this.isLiftFree = false;
@@ -37,9 +38,12 @@ export default {
         transform: `translateY(${100 - floor * 100}px)`,
       };
       this.liftOnFloor = floor;
+      localStorage.setItem('floors', this.floors);
+      localStorage.setItem('liftOnFloor', this.liftOnFloor);
       setTimeout(() => {
-        this.liftRest();                
-        this.liftCallStack.shift();        
+        this.liftRest();
+        this.liftCallStack.shift();
+        localStorage.setItem('liftCallStack', this.liftCallStack);
       }, floorDifference * 1000);
     },
     liftRest() {
@@ -49,11 +53,29 @@ export default {
         this.isLiftFree = true;
       }, 3000);
     },
+    getPreviousState() {
+      (this.floors = Number(localStorage.getItem('floors'))),
+        (this.liftOnFloor = Number(localStorage.getItem('liftOnFloor'))),
+        (this.liftCallStack = localStorage.getItem('liftCallStack').split(',')),
+        (this.liftPosition = {
+          transition: `all 3s linear 0s`,
+          transform: `translateY(${100 - this.liftOnFloor * 100}px)`,
+        });
+      if (this.liftCallStack[0] != '') {
+        console.log(this.liftCallStack);
+        this.moveLiftToFloor(this.liftCallStack[0]);
+      }
+    },
+  },
+  mounted() {
+    if (localStorage.getItem('floors')) {
+      this.getPreviousState();
+    }
   },
   watch: {
-  isLiftRest(newValue){
-    !newValue && this.liftCallStack.length > 0 ? this.moveLiftToFloor(this.liftCallStack[0]) : null
-  }
+    isLiftRest(newValue) {
+      !newValue && this.liftCallStack.length > 0 ? this.moveLiftToFloor(this.liftCallStack[0]) : null;
+    },
   },
 };
 </script>
@@ -61,19 +83,20 @@ export default {
 <template>
   <section class="section__lift">
     <div class="floor" v-for="floor in floors">
-      <div class="lift__shaft" v-for="shaft in shafts"></div>
+      <div class="lift__shaft" v-for="shaft in shafts">
+        <div class="lift" :class="{ lift__blink: isLiftRest }" v-for="shaft in shafts" :style="liftPosition">
+          <p class="lift__direction" v-show="!this.isLiftFree">
+            {{ `${this.liftOnFloor}  ${isLiftDirectionUp ? '↑' : '↓'}` }}
+          </p>
+        </div>
+      </div>
       <button
         class="lift__btn"
-        :class="{ lift__btn_active: liftCallStack.find(element=>element===floor) }"
+        :class="{ lift__btn_active: this.liftCallStack.find((element) => element === floor) }"
         @click="callLift(floor)"
       >
         {{ floor }}
       </button>
-    </div>
-    <div class="lift" :class="{ lift__blink: isLiftRest }" v-for="shaft in shafts" :style="liftPosition">
-      <p class="lift__direction" v-show="!this.isLiftFree">
-        {{ `${this.liftOnFloor}  ${isLiftDirectionUp ? '↑' : '↓'}` }}
-      </p>
     </div>
   </section>
 </template>
@@ -101,10 +124,14 @@ export default {
   border-bottom: 1px solid gray;
 }
 .lift__shaft {
+  margin-left: 10px;
   width: 100px;
   height: 98px;
   border-left: 1px solid gray;
   border-right: 1px solid gray;
+}
+.lift__shaft:first-of-type {
+  margin-left: 0;
 }
 .lift__shaft_full {
   background-color: lightblue;
