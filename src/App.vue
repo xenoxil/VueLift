eb1b63c572a781fc8f626bcb5d00e32e9d4079c2
Xenoxil <xilus1127@gmail.com>
2022-10-14 11:52:30 +0300

    сделать вёрстку лифта

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,86 @@
-<script setup>
-
+<script>
+import Floor from '@/components/floor.vue';
+export default {
+  components: {
+    Floor,
+  },
+  data() {
+    return {
+      floors: 5,
+      liftCallStack: [],
+    };
+  },
+  methods: {
+    callLift() {},
+  },
+};
 </script>
 
-<template>
-<section class="section__lift"> 
-  <div class="floor">
-    <div class="lift__shaft"></div>
-    <button class="lift__btn"></button>
-  </div>
-  
-</section>
+<template>  
+    <section class="section__lift">
+      <div class="floor" v-for="floor in floors">
+        <div class="lift__shaft"></div>
+        <button
+          class="lift__btn"
+          @click="
+            {
+            }
+          "
+        >
+          {{ floor }}
+        </button>
+      </div>
+      <div class="lift"></div>
+    </section>  
 </template>
 
-<style >
-*{margin:0;
-padding: 0;
-box-sizing: border-box;}
-.section__lift{
-  width: 100vw;
-  height:100vh;
-background-color: rgb(231, 231, 231);
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
-.floor{
-  width:100px;
+.section__lift {
+  margin:auto 0;
+  width: 100vw;  
+  background-color: rgb(231, 231, 231);
+  position: relative;
+  display: flex;
+  flex-direction: column-reverse;
+}
+.floor {
+  display: flex;
+  flex-direction: row;
+  margin-left: 15px;
+  width: calc(100%-15px);
   min-height: 100px;
+  border-bottom: 1px solid gray;
 }
-.lift__shaft{
-  margin-left:15px;
-  width:100%;
-  min-height: 100%;
-  border-left:  1px solid gray;
-  border-right:  1px solid gray;
+.lift__shaft {
+  width: 100px;
+  height: 98px;
+  border-left: 1px solid gray;
+  border-right: 1px solid gray;
+}
+.lift__shaft_full {
+  background-color: lightblue;
+}
+.lift__btn {
+  margin: auto 0 auto 15px;
+  border-radius: 10px;
+  background-color: white;
+  box-shadow: 1px black;
+  width: 20px;
+  height: 20px;
+}
+.lift {
+  position: absolute;
+  left: 15px;
+  bottom: 0px;
+  background-color: lightblue;
+  width: 100px;
+  height: 100px;
+  transition: all 3s linear 0s;
+  transform: translateY(0px);
 }
 </style>
---
+++ src/components/floor.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="floor">
+    <div class="lift__shaft"></div>
+    <div class="floor__container">
+      <p class="floor__number">{{ floor.id }}</p>
+      <button class="lift__btn">liftButton</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        floor:{
+            type:Object,
+            required:true,
+        }
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
